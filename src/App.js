f4fbc1cf5b893aad51112b19ce8ec069e91aa3e3
Chloé Adrian <58774068+chloeadr@users.noreply.github.com>
2020-10-13 17:49:15 +0200

    Added alt text to the logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       {/* Header */} 
       <nav class="navbar navbar-expand-lg navbar-light menu">
         <a class="navbar-brand" href="index.html">
-          <img src="img/logo.svg" />
+          <img src="img/logo.svg" alt="logo"/>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
