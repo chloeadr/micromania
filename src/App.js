2b99f8c794d866f914a835973d33133a54731fe5
Chloé Adrian <58774068+chloeadr@users.noreply.github.com>
2020-10-13 17:32:27 +0200

    Added a carousel that is now somehow working

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
       </div>
 
       {/* Carousel */}
-      <div id="#carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" interval="10000">
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img class="d-block w-100" src="img/fifa21.jpg" alt="First slide" />
@@ -81,11 +81,11 @@ function App() {
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon carouselControl" aria-hidden="true"></span>
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon carouselControl" aria-hidden="true"></span>
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
       </div>
